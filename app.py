c1b334756c62b8c2167cabcb9b585d93b75f0bf7
Aniket Chaudhary <iamani7464@gmail.com>
2025-01-17 14:58:50 +0530

    Small fix

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import os
 import json
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 groq_api = os.getenv("GROQ_API_KEY")
 
 st.sidebar.title("Model Selection")
